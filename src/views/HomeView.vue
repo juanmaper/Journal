daedd2e5939c70ae958b8f09314d231c1071c8ea
Juanma <juanmaper30@gmail.com>
2022-06-02 16:33:35 +0200

    Add directory structure, layout for the daybook, modularize router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,18 @@
 
   <h1>Hola mundo</h1>
   
-  <button class="btn btn-primary">Primary</button>
-  <button class="btn btn-secondary">Secondary</button>
-  <button class="btn btn-success">Success</button>
+  <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+  <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+  <button @click="goToDaybook" class="btn btn-success">Success</button>
 </template>
+
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'daybook' })
+    }
+  }
+}
+</script>
