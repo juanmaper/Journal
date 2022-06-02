5ebd9b36fa6ba5af92f74d81eb1ce08f682b645e
Juanma <juanmaper30@gmail.com>
2022-06-02 17:40:23 +0200

    Add entryList and entry components, styles

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+
+    <!-- Titulo -->
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">2</span>
+      <span class="mx-1 fs-5">Junio</span>
+      <span class="mx-2 fw-light">2022</span>
+    </div>
+
+    <div class="entry-description">
+      Culpa duis ea anim nostrud qui aliquip ut sit est magna. Reprehenderit dolore deserunt laborum ad. Amet fugiat labore eu do sunt do fugiat sint aliquip do cupidatat sit elit. Tempor veniam incididunt laboris aliquip esse incididunt cupidatat.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover {
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+
+  .entry-description {
+    font-size: 12px;
+  }
+}
+
+
+</style>
