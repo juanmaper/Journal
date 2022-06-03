204e3a389747902d176bb912b231906d6897939a
Juanma <juanmaper30@gmail.com>
2022-06-03 17:57:15 +0200

    Add fake entries just for now

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,24 @@
 
 export default () => ({
   isLoading: true,
-  entries: [1,2,3,4,5,6,7]
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Dolor tempor tempor aute minim cillum. Nostrud do exercitation ad laboris sint. Commodo proident enim amet nulla velit esse veniam eu aliquip tempor dolore ullamco labore. Consectetur consectetur minim amet sint commodo velit incididunt est ex eiusmod ipsum cupidatat. Officia velit enim mollit amet id fugiat quis laborum Lorem minim ipsum.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: 'Non dolore dolor ad elit eiusmod Lorem laborum commodo. Mollit ipsum officia non dolor ad Lorem in proident eu. Reprehenderit duis excepteur in fugiat do voluptate aliqua elit consectetur sit ipsum. Dolor fugiat nulla amet incididunt ex incididunt id magna nulla labore non Lorem sint adipisicing. Dolor magna incididunt occaecat aliquip sunt laborum fugiat culpa. Qui culpa consequat culpa minim aliqua consequat mollit.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: 'Officia fugiat amet duis consequat. Ad ut tempor eu mollit dolore et nulla id. Ad veniam in laboris elit ut quis deserunt labore fugiat. Mollit anim magna exercitation laboris. Dolore do qui consectetur dolore magna amet dolore aliqua.',
+      picture: null
+    }
+  ]
 })
