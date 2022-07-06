0bf7c4e51c822843471649adb95d0ac94b8f5c30
Juanma <juanmaper30@gmail.com>
2022-07-06 18:13:47 +0200

    Form registration managemenet and create useAuth composable

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -2,25 +2,26 @@
   <span class="login100-form-title p-b-41">
     Register
   </span>
-  <form class="login100-form validate-form p-b-33 p-t-5">
+  <form class="login100-form validate-form p-b-33 p-t-5"
+    @submit.prevent="onSubmit">
 
     <div class="wrap-input100 validate-input" data-validate = "Enter username">
-      <input class="input100" type="text" placeholder="Username" required>
+      <input v-model="userForm.username" class="input100" type="text" placeholder="Username" required>
       <span class="focus-input100" data-placeholder="&#xe82a;"></span>
     </div>
 
     <div class="wrap-input100 validate-input" data-validate = "Enter email">
-      <input class="input100" type="text" placeholder="Email" required>
+      <input v-model="userForm.email" class="input100" type="email" placeholder="Email" required>
       <span class="focus-input100" data-placeholder="&#xe818;"></span>
     </div>
 
     <div class="wrap-input100 validate-input" data-validate="Enter password">
-      <input class="input100" type="password" placeholder="Password" required>
+      <input v-model="userForm.password" class="input100" type="password" placeholder="Password" required>
       <span class="focus-input100" data-placeholder="&#xe80f;"></span>
     </div>
 
     <div class="container-login100-form-btn m-t-32">
-      <button class="login100-form-btn">
+      <button type="submit" class="login100-form-btn">
         Create account
       </button>
     </div>
@@ -33,7 +34,29 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+import useAuth from '../composables/useAuth'
+
 export default {
+
+  setup() {
+
+    const { createUser } = useAuth()
+
+    const userForm = ref({
+      username: '',
+      email: '',
+      password: '',
+    })
+
+    return {
+      userForm,
+
+      onSubmit: async() => {
+        createUser( userForm.value )
+      }
+    }
+  }
 
 }
 </script>
