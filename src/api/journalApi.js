63a87b8042b8dd982c7fdfbb2140b1c608cb51bc
Juanma <juanmaper30@gmail.com>
2022-07-08 16:51:14 +0200

    Commented the token part of firebase as their rules are not working properly, so its better not to add the token in the request for now

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,13 +4,14 @@ const journalApi = axios.create({
   baseURL: 'https://vue-demos-2cb4d-default-rtdb.europe-west1.firebasedatabase.app',
 })
 
+// This could be done in the above part I think, but the using of interceptor to show it is interesting. 
+// Anyway, everything is commented as firebase does not get configured properly in terms of rules and auth
+
 // journalApi.interceptors.request.use( ( config ) => {
 
 //   config.params = {
 //     auth: localStorage.getItem('idToken')
 //   }
-
-//   console.log(config);
 
 //   return config
 
