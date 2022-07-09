e357344a0a6e025c8298b73456e288499f80e99e
Juanma <juanmaper30@gmail.com>
2022-07-09 13:30:08 +0200

    Test for vuex auth store mutation: loginUser

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -18,6 +18,33 @@ describe('Vuex: auth module tests', () => {
     expect( user ).toBe( null )
     expect( idToken ).toBe( null )
     expect( refreshToken ).toBe( null )
-})
+  })
+
+  // Mutations
+  test('Mutation: loginUser', () => {
+        
+    const store = createVuexStore({
+        status: 'authenticating', // 'authenticated','not-authenticated', 'authenticating'
+        user: null,
+        idToken: null,
+        refreshToken: null
+    })
+
+    const payload = {
+        user: { name: 'User', email: 'user@gmail.com' },
+        idToken: 'ABC-123',
+        refreshToken: 'XYZ-123'
+    }
+
+    store.commit('auth/loginUser', payload )
+
+    const { status, user, idToken, refreshToken } = store.state.auth
+    
+    expect( status ).toBe( 'authenticated' )
+    expect( user ).toEqual( { name: 'User', email: 'user@gmail.com' } )
+    expect( idToken ).toBe( 'ABC-123' )
+    expect( refreshToken ).toBe( 'XYZ-123' )
+
+  })
 
 })
