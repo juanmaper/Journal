2f1ae90702a1f2ae45c540b77ad3fed7c8617364
Juanma <juanmaper30@gmail.com>
2022-07-11 12:52:57 +0200

    Router tests in navbar composition api

--- tests/unit/modules/daybook/components/navbar.spec.js
+++ tests/unit/modules/daybook/components/navbar.spec.js
@@ -3,6 +3,13 @@ import { shallowMount } from '@vue/test-utils'
 import NavBar from '@/modules/daybook/components/NavBar.vue'
 import createVuexStore from '../../../mocks-data/mock-store'
 
+const mockRouter = {
+  push: jest.fn()
+}
+
+jest.mock('vue-router', () => ({
+  useRouter: () => mockRouter
+}))
 
 describe('Navbar component tests', () => {
 
@@ -31,7 +38,26 @@ describe('Navbar component tests', () => {
 
     })
 
-    
+    test('should close session and redirect when the logout is clicked', async() => {
+        
+      const wrapper = shallowMount( NavBar, {
+          global: {
+              plugins: [ store ]
+          }
+      })
+
+      await wrapper.find('button').trigger('click')
+
+      expect( mockRouter.push ).toHaveBeenCalledWith({ name: 'login' })
+
+      expect( store.state.auth ).toEqual({
+          user: null,
+          status: 'not-authenticated',
+          idToken: null,
+          refreshToken: null
+      })
+
+  })
     
     
     
